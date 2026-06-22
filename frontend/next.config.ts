339c0120ddccab9012d3810cb1be39c85ae2a8ab
Imrane Larhrib <imranelarhrib2017@gmail.com>
2026-06-22 18:29:55 +0200

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,6 +5,14 @@ const nextConfig: NextConfig = {
   outputFileTracingIncludes: {
     "/api/chat": ["./node_modules/onnxruntime-node/bin/napi-v6/linux/x64/**/*"],
   },
+  outputFileTracingExcludes: {
+    "/api/chat": [
+      "./node_modules/onnxruntime-node/bin/napi-v6/darwin/**/*",
+      "./node_modules/onnxruntime-node/bin/napi-v6/win32/**/*",
+      "./node_modules/onnxruntime-node/bin/napi-v6/linux/arm64/**/*",
+      "./node_modules/@huggingface/transformers/.cache/**/*",
+    ],
+  },
 };
 
 export default nextConfig;
